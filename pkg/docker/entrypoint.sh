4369182b01abe970ec32e4b24c4471b25eefc26c
Joseph Daudi <josedaudi@gmail.com>
2024-09-13 04:30:34 +0300

    Updated deployment file

--- pkg/docker/entrypoint.sh
+++ pkg/docker/entrypoint.sh
@@ -56,26 +56,12 @@ if [ ! -f /var/lib/pgadmin/pgadmin4.db ] && [ "${external_config_db_exists}" = "
     fi
     
 #    is_valid_email=$(cd /pgadmin4/pgadmin/utils && /venv/bin/python3 -c "from validation_utils import validate_email; val = validate_email('${PGADMIN_DEFAULT_EMAIL}', ${CHECK_EMAIL_DELIVERABILITY}); print(val)")
-#    is_valid_email=$(cd /pgadmin4/pgadmin/utils && /venv/bin/python3 -c 'from validation_utils import validate_email; val = validate_email("'"${PGADMIN_DEFAULT_EMAIL}"'", '"${CHECK_EMAIL_DELIVERABILITY}"'); print(val)')
-#    is_valid_email=$(cd /pgadmin4/pgadmin/utils && /venv/bin/python3 << EOF
-#    from validation_utils import validate_email
-#    email = """${PGADMIN_DEFAULT_EMAIL}"""
-#    check_deliverability = ${CHECK_EMAIL_DELIVERABILITY}
-#    val = validate_email(email, check_deliverability)
-#    print(val)
-#    EOF)
-    is_valid_email=$(cd /pgadmin4/pgadmin/utils && /venv/bin/python3 << EOF
-    from validation_utils import validate_email
-    email = """${PGADMIN_DEFAULT_EMAIL}"""
-    check_deliverability = ${CHECK_EMAIL_DELIVERABILITY}
-    val = validate_email(email, check_deliverability)
-    print(val)
-    EOF
-    )
-    if echo "${is_valid_email}" | grep "False" > /dev/null; then 
-        echo "'${PGADMIN_DEFAULT_EMAIL}' does not appear to be a valid email address. Please reset the PGADMIN_DEFAULT_EMAIL environment variable and try again."
-        exit 1
-    fi
+     is_valid_email=$(cd /pgadmin4/pgadmin/utils && /venv/bin/python3 -c 'from validation_utils import validate_email; val = validate_email('"${PGADMIN_DEFAULT_EMAIL}"', '"${CHECK_EMAIL_DELIVERABILITY}"'); print(val)')
+
+#    if echo "${is_valid_email}" | grep "False" > /dev/null; then
+#        echo "'${PGADMIN_DEFAULT_EMAIL}' does not appear to be a valid email address. Please reset the PGADMIN_DEFAULT_EMAIL environment variable and try again."
+#        exit 1
+#    fi
     # Switch back to root directory for further process
     cd /pgadmin4
 
